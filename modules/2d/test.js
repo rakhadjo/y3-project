5c9818f77c92d46d0bfc2ea6fb0f41bf273223ad
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-02 17:56:24 +0000

    add comments on TODOs

--- modules/2d/test.js
+++ modules/2d/test.js
@@ -1,6 +1,10 @@
 let exp = {
   0: {
     // figure out how to do lhs, comp, and rhs
+    // rethink naming of the keys so that people understand
+    // in 5w time will need to explain in the report, demo
+    // make sure it really works and reads in those rules
+    // rule builder diagram is too detailed
     nextstate: {
       required: {
         0: {
@@ -17,7 +21,7 @@ let exp = {
         },
       },
       satisfied: 1,
-      else: 1,
+      else: 0,
     },
   },
   1: {
@@ -68,6 +72,23 @@ function condition_builder(reqs) {
       : "neighs[" + reqs.lhs.states + "]";
 
     condition = "[" + reqs.rhs.vals + "]" + ".includes(" + rng + ")";
+  } else {
+    // (reqs.lhs.type == "states" && reqs.rhs.type == "states")
+    for (let st in reqs.lhs.states) {
+      rng += rng
+        ? " + " + "neighs[" + reqs.lhs.states[st] + "]"
+        : "neighs[" + reqs.lhs.states[st] + "]";
+    }
+    rng += " " + reqs.cmp + " ";
+    let _rhs = "";
+    for (let _st in reqs.rhs.states) {
+      console.log("state!!!");
+      _rhs += _rhs
+        ? " + " + "neighs[" + reqs.rhs.states[_st] + "]"
+        : "neighs[" + reqs.rhs.states[_st] + "]";
+    }
+
+    condition = rng + _rhs;
   }
 
   return condition;
@@ -104,9 +125,69 @@ function create_function(json_rules) {
   
       }  */
   }
-  //return f;
-  return new Function("neighs", "cur_state", f);
+  return f;
+  //return new Function("neighs", "cur_state", f);
 }
 
-var x = create_function(exp)
-console.log(x.format)
+let expt = {
+  0: {
+    // figure out how to do lhs, comp, and rhs
+    nextstate: {
+      required: {
+        1: {
+          lhs: {
+            type: "states",
+            states: [3, 4],
+          },
+          cmp: ">",
+          rhs: {
+            type: "states",
+            states: [1, 2],
+          },
+          conn: "AND",
+        },
+      },
+      satisfied: 3,
+      else: 1,
+    },
+  },
+  1: {
+    // figure out how to do lhs, comp, and rhs
+    nextstate: {
+      required: {
+        1: {
+          lhs: {
+            type: "states",
+            states: [3, 4],
+          },
+          cmp: ">",
+          rhs: {
+            type: "states",
+            states: [1, 6],
+          },
+          conn: "AND",
+        },
+      },
+      satisfied: 3,
+      else: 1,
+    },
+  },
+};
+
+//console.log(condition_builder(expt[0].nextstate.required[1]));
+console.log(create_function(expt));
+
+let cur_state = 0;
+let neighs = 0;
+
+if (cur_state == 0) {
+  if ([3].includes(neighs[1])) {
+    return 1;
+  }
+  return 0;
+} else if (cur_state == 1) {
+  if ([2, 3].includes(neighs[1])) {
+    return 1;
+  }
+  return 1;
+}
