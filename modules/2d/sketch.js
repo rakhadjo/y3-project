84d7d980d52ba60376acd1200bd59c13897de727
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-03 10:49:08 +0000

    moved functions to util.js

--- modules/2d/sketch.js
+++ modules/2d/sketch.js
@@ -29,11 +29,13 @@ function generateCells(grid, states) {
 function determine_rules(states) {
   switch (states) {
     case 5:
-      return fire_13;
+      return create_function(expt2);
     case 6:
       return fire_1;
     default:
-      return cgol_rules;
+      //2
+      //return create_function(exp2);
+      return create_function(exp3_woo)
   }
 }
 
@@ -41,46 +43,19 @@ function setup(k = 2) {
   // get the number of states
   generationCount = 0;
   let states = parseInt(document.getElementById("states").value) || 2;
-  depth = parseInt(document.getElementById("depth").value) || 1;
-  document.getElementById("states_count").innerHTML =
-    "Number of States: " + states;
-  document.getElementById("depth_count").innerHTML = "Depth: " + depth;
+  depth = parseInt(document.getElementById("depth").value) || 0;
+  announceStates(states);
+  announceDepth(depth);
   active_rules = determine_rules(states);
   colors = colorBank(states);
   cnv = createCanvas(windowWidth / 2, 600);
   centerCanvas();
   background(0);
-  cols = width / resolution;
-  rows = height / resolution;
+  cols = floor(width / resolution);
+  rows = floor(height / resolution);
   grid = make2DArray(cols, rows);
   generateCells(grid, states);
   step();
-}
-
-// generate random colors according to number of states
-function colorBank(states) {
-  if (states == 2) {
-    document.getElementById("colors_bank").innerHTML = "Default Blank & White";
-    return [0, 255];
-  }
-  let bank = [];
-  for (let i = 0; i < states; i++) {
-    let rand_color = [
-      floor(random(255)),
-      floor(random(255)),
-      floor(random(255)),
-    ];
-    while (bank.includes(rand_color)) {
-      rand_color = [floor(random(255)), floor(random(255)), floor(random(255))];
-    }
-    bank.push(rand_color);
-  }
-  let output = "Colors: [";
-  bank.forEach((color) => {
-    output += `(${color[0]}, ${color[1]} , ${color[0]}) `;
-  });
-  document.getElementById("colors_bank").innerHTML = output + "]";
-  return bank;
 }
 
 function draw() {
@@ -92,6 +67,7 @@ function draw() {
       for (let j = 0; j < rows; j++) {
         let x = i * resolution;
         let y = j * resolution;
+        //console.log(`grid[${i}][${j}]: ${grid[i][j]}`)
         fill(colors[grid[i][j]]);
         stroke(0);
         rect(x, y, resolution - 1, resolution - 1);
@@ -130,36 +106,4 @@ function applyRule(current, rules) {
     }
   }
   return next;
-}
-
-// Mouse Functions
-
-function mouseClicked() {
-  //alert(mouseX + ", " + mouseY);
-  //alert(grid[mouseX][mouseY]);
-}
-
-// Button Functions
-
-function step() {
-  pause = false;
-  draw();
-  console.log("")
-  pause = true;
-}
-
-function setPause() {
-  pause = !pause;
-}
-
-// util functions
-
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
-}
-
-function windowResized() {
-  centerCanvas();
 }
