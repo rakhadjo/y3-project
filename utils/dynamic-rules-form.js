6c37b927c85520c541e20875611ec487d3c31142
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-10 03:20:18 +0000

    render some of the form

--- utils/dynamic-rules-form.js
+++ utils/dynamic-rules-form.js
@@ -3,8 +3,21 @@ function renderFormStates(states, formID) {
   let inp = "";
   formElem.innerHTML = inp;
   for (let i = 0; i < states; i++) {
-      console.log(`${colors[i]}`)
-      let nxt = `<label style="color: rgb(${colors[i]})"> state ${i}</label>`
+    console.log(`${colors[i]}`);
+    let nxt = `<label style="color: rgb(${colors[i]})"> state ${i}</label>`;
+    let inp_rad = `
+    <br> 
+    <label for="${i}_nextstate">Next State: 
+    <input type="text" id="${i}_nextstate"> <br /> <br />
+    <input checked value="totalling" type="radio" id="${i}_totalling" name="${i}_ruletype" onChange="handleTypeChange(this, ${i})"> 
+    <label for=${i}_totalling>Totalling</label> 
+    <input value="expression" type="radio" id="${i}_expression" name="${i}_ruletype" onChange="handleTypeChange(this, ${i})"> 
+    <label for=${i}_expression>Espression</label>`;
+
+    nxt += inp_rad;
+
+    //nxt += getRuleInput("totalling", i, states)
+
     inp += inp ? "</br>" + nxt : nxt;
   }
   formElem.innerHTML = inp;
