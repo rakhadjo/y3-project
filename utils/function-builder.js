a4d2385cfa5d509fc9e515053ba222e7540c3dd0
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-16 21:58:04 +0000

    fix condition builder to handle mutliple reqs

--- utils/function-builder.js
+++ utils/function-builder.js
@@ -13,46 +13,44 @@ function if_builder(condition, satisfied_rtn, otherwise) {
   let rtn = "";
   if (typeof otherwise == "number") {
     rtn += `if (${condition}) { return ${satisfied_rtn} } return ${otherwise}`;
-  } else {
-    // typeof otherwise == 'object'
-    let cond_2 = condition_builder(otherwise.conditional_requirements[0]);
-    rtn += `if (${condition}) { return ${satisfied_rtn} } else if (${cond_2}) { return ${otherwise.satisfied} } else { return ${otherwise.else} }`;
   }
   return rtn;
 }
 
-function condition_builder(reqs) {
-  //console.log(reqs)
+function condition_builder(requirements) {
   let condition = "";
   let rng = "";
-  if (reqs.hasOwnProperty("type")) {
-    if (reqs.type == "totalling") {
-      rng = `neighs[${reqs.neighbour_state}]`;
-      condition = `[${reqs.total}].includes(${rng})`;
-    } else if (reqs.type == "expression") {
-      for (let st in reqs.lhs.neighbour_states) {
-        let nxt = `neighs[${reqs.lhs.neighbour_states[st]}]`;
-        rng += rng ? ` + ${nxt}` : `${nxt}`;
+  let inner_cond = "";
+  for (let reqs in requirements) {
+    if (requirements[reqs].hasOwnProperty("type")) {
+      if (requirements[reqs].type == "totalling") {
+        rng = `neighs[${requirements[reqs].neighbour_state}]`;
+        inner_cond = `[${requirements[reqs].total}].includes(${rng})`;
+      } else if (requirements[reqs].type == "expression") {
+        for (let st in requirements[reqs].lhs.neighbour_states) {
+          let nxt = `neighs[${requirements[reqs].lhs.neighbour_states[st]}]`;
+          rng += rng ? ` + ${nxt}` : `${nxt}`;
+        }
+        rng += ` ${requirements[reqs].cmp} `;
+        let _rhs = "";
+        for (let _st in requirements[reqs].rhs.neighbour_states) {
+          let _nxt = `neighs[${requirements[reqs].rhs.neighbour_states[_st]}]`;
+          _rhs += _rhs ? ` + ${_nxt}` : `${_nxt}`;
+        }
+        inner_cond = rng + _rhs;
+      } else {
+        // (reqs.type == "probability")
+        inner_cond = `probability(${requirements[reqs].p})`;
       }
-      rng += ` ${reqs.cmp} `;
-      let _rhs = "";
-      for (let _st in reqs.rhs.neighbour_states) {
-        let _nxt = `neighs[${reqs.rhs.neighbour_states[_st]}]`;
-        _rhs += _rhs ? ` + ${_nxt}` : `${_nxt}`;
-      }
-      condition = rng + _rhs;
+      condition += condition ? "&& " + inner_cond : inner_cond;
     }
-     else {
-       // (reqs.type == "probability")
-       condition = `probability(${reqs.p})`
-     }
   }
 
   return condition;
 }
 
 function default_builder(def) {
-  let check = condition_builder(def.conditional_requirements[0]);
+  let check = condition_builder(def.conditional_requirements);
   return if_builder(check, def.satisfied, def.else);
 }
 
@@ -68,20 +66,18 @@ function create_function(json_rules) {
       // not the `default` value
       //console.log(`checking: ${json_rules[key].next}`);
       let top_if = `if (cur_state == ${key}) {`;
-      f += f ? "else " + top_if : top_if;
+      f += f ? " else " + top_if : top_if;
       let inner_if = "";
-      for (let requirement in json_rules[key].next.conditional_requirements) {
-        let check = condition_builder(
-          json_rules[key].next.conditional_requirements[requirement]
-        );
-        let cond = if_builder(
-          check,
-          json_rules[key].next.satisfied,
-          json_rules[key].next.else
-        );
-        inner_if += inner_if ? "else " + cond : cond;
-        f += inner_if + " }";
-      }
+      let check = condition_builder(
+        json_rules[key].next.conditional_requirements
+      );
+      let cond = if_builder(
+        check,
+        json_rules[key].next.satisfied,
+        json_rules[key].next.else
+      );
+      inner_if += inner_if ? " else " + cond : cond
+      f += inner_if + " }"
     }
 
     //f += "}";
