7aa341a2b5bd3c8f6960fbb591af537b03d434c1
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-16 15:24:39 +0000

    add fire simulator json rules

---
+++ defaults/firesim.js
@@ -0,0 +1,41 @@
+// forest fire simulation!
+
+/**
+ * 1. a cell that cannot be burned stays the same
+ * 2. a cell that is burning down at present time will be completely burned in the next step
+ * 3. a burned cell cannot burn again
+ * 4. if a cell is burning down at the present time and there are next-nearest neigbor cells containing fuel, the fire can propagae to its neighbors with probability p_burn
+ */
+
+/**
+ * 0: burnt or unable to burn
+ * 1: burning down
+ * 2: burnable
+ */
+
+let firesim = {
+  default: {
+    next: {
+      conditional_requirements: [
+        {
+          type: "probability",
+          p: 1,
+        },
+      ],
+      satisfied: 0,
+      else: 0,
+    },
+  },
+  2: {
+    next: {
+      conditional_requirements: [
+        {
+          type: "probability",
+          p: 0.4,
+        },
+      ],
+      satisfied: 1,
+      else: 2,
+    },
+  },
+};
