a337f8611ea20de501b3c216e23ea6185acd70ef
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-22 03:35:51 +0000

    yass ideal width

--- utils/dynamic-rules-form.js
+++ utils/dynamic-rules-form.js
@@ -13,7 +13,9 @@ function renderFormStatesFromActiveRules(json_rules, formID) {
   let formInput = `
   <br />
   <label for="rules_textarea">Rules:</label> <br />
-  <textarea id="rules_textarea" name="rules_textarea" rows="4" cols="50">
+  <textarea id="rules_textarea" name="rules_textarea" rows="${
+    json_rules.split(/\r\n|\r|\n/).length
+  }" cols="80">
     ${json_rules}
   </textarea>
     <br><br>
