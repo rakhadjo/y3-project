28af4e065f8aef0e66ddcb339b8623a3b85f6b15
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-03-25 14:41:22 +0000

    add schema option argument

--- utils/validator.js
+++ utils/validator.js
@@ -2,7 +2,7 @@ import("../libraries/tv4");
 
 // only one schema is there, so will only input rules
 // return some feed back on rules validation if bad
-function obeysJsonSchema(json_rules) {
+function obeysJsonSchema(json_rules, schema = schema) {
   try {
     let k = JSON.parse(json_rules);
     return tv4.validate(k, schema); // nice implementation, now i just gotta implement the schema
