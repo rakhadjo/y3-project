04f2181305270515ec39a84c32b9ecbf4f53cd23
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-04-11 15:57:59 +0700

    working validator with feedback

--- utils/validator.js
+++ utils/validator.js
@@ -6,9 +6,13 @@ function obeysJsonSchema(json_rules, schema_select = schema) {
     console.log(schema_select)
     try {
         let k = JSON.parse(json_rules);
-        let v_results = tv4.validate(k, schema_select, false, false)
-        alert("valuidator resdfsdfsults: " + v_results)
-        return tv4.validate(k, schema_select);
+        let v_results = tv4.validate(k, schema_select)
+        let output = `validator results: ${v_results} \n ${tv4.error}`
+        if (tv4.error && tv4.error.dataPath) {
+            output += `\n ${tv4.error} at: ${tv4.error.dataPath}`
+        }
+        alert(output)
+        return v_results;
     } catch(err) {
         console.log("custom error: " + err)
         return false
@@ -18,18 +22,3 @@ function obeysJsonSchema(json_rules, schema_select = schema) {
 
 // only one schema is there, so will only input rules
 // return some feed back on rules validation if bad
-function obeysJsonSchema2(json_rules, schema_select = schema) {
-  try {
-    let k = JSON.parse(json_rules);
-    if (tv4.validate(k, schema_select)) {
-        return true;
-    } else {
-        console.log("validator error: " + tv4.error.dataPath)
-        return false;
-    }
-    return tv4.validate(k, schema_select); // nice implementation, now i just gotta implement the schema
-  } catch (err) {
-    console.log("err: " + err)
-    return false;
-  }
-}
