6316d1101fb2dacb29d9278894cde81f85aadaa1
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-04-11 14:58:56 +0700

    delete unused lines

--- utils/schemas/schema.js
+++ utils/schemas/schema.js
@@ -1,42 +1,3 @@
-let schema_state = {
-  name: "state",
-  type: "object",
-  properties: {
-    next: {
-      type: "object",
-    },
-  },
-};
-
-let schema_next = {
-  name: "next",
-  type: "object",
-  properties: {
-    conditional_requirements: {
-      type: "array",
-      items: "",
-    },
-    satisfied: {
-      type: "boolean",
-    },
-    else: {
-      type: "boolean",
-    },
-  },
-};
-
-let schema_cond_reqs = {
-  name: "conditional_requirement",
-  type: "object",
-  properties: {
-    type: {
-      enum: ["totalling", "total-p", "probability", "expression"],
-    },
-  },
-  required: ["type"],
-  additionalproperties: true,
-};
-
 let schema = {
   type: "object",
   properties: {
@@ -171,6 +132,5 @@ let schema = {
       ],
     },
   },
-
   required: ["$_meta"],
 };
