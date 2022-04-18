12f3a8f16d9ea4c87999ed3f5eca30dda96240cf
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-04-19 03:08:54 +0700

    implement functionality: user-specified colors

--- utils/sketcher.js
+++ utils/sketcher.js
@@ -9,24 +9,38 @@ function genRandomColor() {
 }
 
 // generate random colors according to number of states
-function colorBank(states, fireMode = false) {
+function colorBank(states, fireMode = false, colors_map = null) {
+  if (colors_map) {
+    if (Object.keys(colors_map).length == states) {
+      let bank = [];
+      for (let i = 0; i < states; i++) {
+        if (isColor(colors_map[i])) {
+          bank.push(colors_map[i]);
+        } else {
+          bank.push(genRandomColor());
+        }
+      }
+      announceColors("custom user defined colors: " + bank);
+      return bank;
+    }
+  }
   if (states == 2) {
     announceColors("Default Black & White");
     return [0, 255];
   } else if (fireMode && states > 2) {
     announceColors("Forest Fire Mode");
     // 0: burnt, 1: burnable, 2: burning
-    return [[0, 0, 0], [32, 168, 5], [255, 0, 0]];
+    return [
+      [0, 0, 0],
+      [32, 168, 5],
+      [255, 0, 0],
+    ];
     // can accept string colors, hex, or 3-RGB values. thats cool
     // return ["#DC143C", "#DC143C", "#DC143C"]
   }
   let bank = [];
   for (let i = 0; i < states; i++) {
-    let rand_color = [
-      floor(random(255)),
-      floor(random(255)),
-      floor(random(255)),
-    ];
+    let rand_color = genRandomColor();
     while (bank.includes(rand_color)) {
       rand_color = [floor(random(255)), floor(random(255)), floor(random(255))];
     }
